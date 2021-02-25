e06ce2b39fe3254e14c8242ca8bb3148bf17aced
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2021-02-24 21:05:40 -0300

    Day 3 Finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import Head from "next/head";
+
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from '../components/Profile';
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
-
-import Head from 'next/head';
-
-import styles from '../styles/pages/Home.module.css'
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
@@ -15,22 +15,19 @@ export default function Home() {
         <title>Início | move.it</title>
       </Head>
 
-      <ExperienceBar/>
-      
+      <ExperienceBar />
+
       <section>
         <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
-
     </div>
-    
-
-  )
+  );
 }
