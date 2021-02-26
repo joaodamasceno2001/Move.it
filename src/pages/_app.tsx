d908668d529c6e4b3c6a8cf54625c1f6ce23399f
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2021-02-26 17:35:45 -0300

    Day 5 completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,7 @@ import { ChallengesProvider } from "../contexts/ChallengesContext";
 import { CountdownProvider } from "../contexts/CountdownContext";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <CountdownProvider>
-        <Component {...pageProps} />
-      </CountdownProvider>
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
